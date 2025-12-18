06bb9e790ec79b6de25bad2c04d3c7b893c919b2
njukenanli <karinali20011210@gmail.com>
2025-12-18 19:09:52 +0800

    cancel mnt in launch from base image because it prevents git clone

--- launch/core/runtime.py
+++ launch/core/runtime.py
@@ -647,12 +647,6 @@ function prompt {
             },
             working_dir=working_dir,
             extra_hosts=extra_hosts,
-            volumes={
-                os.path.join(os.getcwd(), "tmp"): {
-                    "bind": os.path.join(working_dir, "tmp"),
-                    "mode": "rw",
-                }
-            },
             **run_kwargs,
         )
 
@@ -714,8 +708,8 @@ if (-not (Get-Command git.exe -ErrorAction SilentlyContinue)) {
             res: CommandResult = session.send_command(
                 r'git clone {url} "C:\testbed"; cd "C:\testbed"; git reset --hard {base}'.format(
                     url=url, base=base_commit
+                )
             )
-        )
         else: 
             session.send_command("apt update && apt install -y git")
             res: CommandResult = session.send_command(
